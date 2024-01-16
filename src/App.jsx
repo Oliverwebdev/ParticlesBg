2d8072fdff465dff1559bde720b4621beb1c9bba
dci-student <oliver.sporl@dci-student.org>
2024-01-16 13:37:53 +0100

    deploy 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // In deiner App.jsx oder einer ähnlichen Hauptkomponente
 import React from "react";
 import Background from "./components/Design";
+import BackgroundBg from "./components/Different";
 
 
 const App = () => {
     return (
         <div style={{ position: "relative", minHeight: "100vh" }}>
-            <Background /> {/* Die Partikel-Komponente als Hintergrund */}
+            <BackgroundBg /> {/* Die Partikel-Komponente als Hintergrund */}
+            {/* <Background /> Die Partikel-Komponente als Hintergrund */}
             <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
                 <h1>Dein Titel hier</h1>
                 <h2>Etwas Text für den Inhalt</h2>
